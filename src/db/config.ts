5247326e73c953041426f72c93d188fc062f978b
Manuel Pandolfi <manuel.pandolfi@devbase.us>
2021-10-12 09:04:09 -0300

    Debug connection

--- src/db/config.ts
+++ src/db/config.ts
@@ -3,10 +3,10 @@ import mongoose from 'mongoose';
 export const connectDB = () => {
   return new Promise<void>((resolve, _) => {
     if (process.env.NODE_ENV === 'local') {
+      const conn = `mongodb://${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DB}`;
+      console.log('Connecting to...', conn);
       mongoose
-        .connect(
-          `mongodb://${process.env.MONGODB_HOST}:${process.env.PORT}/${process.env.MONGODB_DB}`
-        )
+        .connect(conn)
         .then(() => {
           console.log(`Connected to MongoDb in ${process.env.NODE_ENV} environment...`);
           resolve();
@@ -15,15 +15,18 @@ export const connectDB = () => {
           throw new Error(`Error initializing databse ${err}`);
         });
     } else {
+      const conn = `mongodb://${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}`;
+      const connOptions = {
+        auth: {
+          username: process.env.MONGODB_USER,
+          password: process.env.MONGODB_PASSWORD,
+        },
+        authSource: 'admin',
+        dbName: process.env.MONGODB_DB,
+      };
+      console.log('Connecting to...', conn, connOptions);
       mongoose
-        .connect(`mongodb://${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}`, {
-          auth: {
-            username: process.env.MONGODB_USER,
-            password: process.env.MONGODB_PASSWORD,
-          },
-          authSource: 'admin',
-          dbName: process.env.MONGODB_DB,
-        })
+        .connect(conn, connOptions)
         .then(() => {
           console.log(`Connected to MongoDb in ${process.env.NODE_ENV} environment...`);
           resolve();
