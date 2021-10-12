8e446e25547d8a49d299e6190384c1d77e83980b
mpandolfi-devbase <90272035+mpandolfi-devbase@users.noreply.github.com>
2021-10-12 11:51:04 -0300

    Update Mongo DB Configs - Merge develop into qa (#16)

--- environment.d.ts
+++ environment.d.ts
@@ -1,15 +1,19 @@
 declare global {
-    namespace NodeJS {
-      interface ProcessEnv {
-        GITHUB_AUTH_TOKEN: string;
-        NODE_ENV: 'development' | 'production';
-        PORT?: string;
-        PWD: string;
-        MONGODB_CNN: string
-      }
+  namespace NodeJS {
+    interface ProcessEnv {
+      GITHUB_AUTH_TOKEN: string;
+      NODE_ENV: 'dev' | 'production' | 'local' | 'qa';
+      PORT?: string;
+      PWD: string;
+      MONGODB_USER: string;
+      MONGODB_PASSWORD: string;
+      MONGODB_HOST: string;
+      MONGODB_PORT: string;
+      MONGODB_DB: string;
     }
+  }
 }
-  
+
 // If this file has no import/export statements (i.e. is a script)
 // convert it into a module by adding an empty export statement.
-export {}
+export {};
--- src/db/config.ts
+++ src/db/config.ts
@@ -1,15 +1,40 @@
 import mongoose from 'mongoose';
 
 export const connectDB = () => {
-    const DB = process.env.MONGODB_CNN || '';
-    return new Promise<void>((resolve, _) => {
-        mongoose.connect(DB)
-          .then(() => {
-              console.log(`Connected to MongoDb in ${process.env.NODE_ENV} environment...`);
-              resolve();
-           })
-          .catch((err) => { throw new Error(`Error initializing databse ${err}`) });
-    });
+  return new Promise<void>((resolve, _) => {
+    if (process.env.NODE_ENV === 'local') {
+      const conn = `mongodb://${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DB}`;
+      console.log('Connecting to...', conn);
+      mongoose
+        .connect(conn)
+        .then(() => {
+          console.log(`Connected to MongoDb in ${process.env.NODE_ENV} environment...`);
+          resolve();
+        })
+        .catch((err) => {
+          throw new Error(`Error initializing databse ${err}`);
+        });
+    } else {
+      const conn = `mongodb://${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}`;
+      const connOptions = {
+        auth: {
+          username: process.env.MONGODB_USER,
+          password: process.env.MONGODB_PASSWORD,
+        },
+        authSource: 'admin',
+        dbName: process.env.MONGODB_DB,
+      };
+      mongoose
+        .connect(conn, connOptions)
+        .then(() => {
+          console.log(`Connected to MongoDb in ${process.env.NODE_ENV} environment...`);
+          resolve();
+        })
+        .catch((err) => {
+          throw new Error(`Error initializing databse ${err}`);
+        });
+    }
+  });
 };
 
 export const disconnectDB = () => mongoose.disconnect();
